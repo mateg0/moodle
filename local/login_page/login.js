d972767b3597ab587ccc5d0348464c9d53040a1a
moodle-server <nomail>
2021-10-13 19:41:21 +0300

    Added setup waiting theme for login page in login.js

--- local/login_page/login.js
+++ local/login_page/login.js
@@ -1,8 +1,30 @@
 const urlPostRequest = 'https://edu.bonch-ikt.ru/login/index.php';
-const urlAfterCompletedAuthorization = 'https://edu.bonch-ikt.ru/my';
+const urlAfterSuccessfulAuthorization = 'https://edu.bonch-ikt.ru/';
 
-document.getElementsByClassName('btn-login')[0].addEventListener('click', async (event) => {
+const loginButton = document.getElementsByClassName('btn-login')[0];
+const loginSpinner = document.getElementsByClassName('spinner-login')[0];
+const loginWrap = document.getElementsByClassName('login_wrap')[0];
+
+loginButton.addEventListener('click', async (event) => {
     event.preventDefault();
+
+    const showAuthorizationError = () => {
+
+    };
+
+    const setWaitingThemeLoginPage = () => {
+        loginButton.disabled = true;
+        loginSpinner.classList.add('show-spinner');
+        loginWrap.classList.add('brightless-login-wrap');
+    };
+
+    const removeWaitingThemeLoginPage = () => {
+        loginButton.false = true;
+        loginSpinner.classList.remove('show-spinner');
+        loginWrap.classList.remove('brightless-login-wrap');
+    }
+
+    setWaitingThemeLoginPage();
 
     const loginFormData = new FormData(document.getElementsByClassName('loginform')[0]);
     const response = await fetch(urlPostRequest, {
@@ -12,6 +34,9 @@ document.getElementsByClassName('btn-login')[0].addEventListener('click', async 
     const responseText = await response.text();
 
     if(responseText.includes('Личный кабинет')){
-        window.location.href = urlAfterCompletedAuthorization;
+        window.location.href = urlAfterSuccessfulAuthorization;
+    } else {
+        removeWaitingThemeLoginPage();
+        showAuthorizationError();
     }
 });
