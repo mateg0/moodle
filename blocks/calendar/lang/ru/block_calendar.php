639904f5b663fbebe3344e28a38e89758ba0e5a4
moodle-server <nomail>
2022-05-06 13:26:41 +0300

    added ru strings for calendar and student_involvement blocks

---
+++ blocks/calendar/lang/ru/block_calendar.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Большой календарь';
+$string['calendar'] = 'Календарь';
---
+++ blocks/student_involvement/lang/ru/block_student_involvement.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Вовлеченность студента';
+$string['student_involvement'] = 'Моя активность';
