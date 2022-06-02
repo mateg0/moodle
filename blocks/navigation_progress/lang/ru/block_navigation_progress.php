4c9b9c4fb2f86a2cc38093c744c596b1d9c4f975
moodle-server <nomail>
2022-06-02 16:55:40 +0300

    navigation progress block lang/ru files added

--- blocks/navigation_progress/lang/ru/block_navigation_progress.php
+++ blocks/navigation_progress/lang/ru/block_navigation_progress.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,90 +12,89 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Navigation Progress block Russian language translation
+ * Strings for component 'block_navigation_progress', language 'ru', version '1.0'.
  *
  * @package    block_navigation_progress
  * @copyright  2021 Nikolay Terekhin
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// $string['completed_colour'] = '#73A839';
-// $string['completed_colour_descr'] = 'HTML Colour code for elements that have been completed';
-// $string['completed_colour_title'] = 'Completed colour';
-// $string['completion_not_enabled'] = 'Completion tracking is not enabled on this site.';
-// $string['completion_not_enabled_course'] = 'Completion tracking is not enabled in this course.';
-// $string['navigation_progress:addinstance'] = 'Add a new Navigation Progress block';
-// $string['navigation_progress:myaddinstance'] = 'Add a Navigation Progress block to My home page';
-// $string['navigation_progress:overview'] = 'View course overview of Navigation Progress for all students';
-// $string['navigation_progress:showbar'] = 'Show the bar in the Navigation Progress block';
-// $string['config_activitiesincluded'] = 'Activities included';
-// $string['config_activitycompletion'] = 'All activities with completion set';
-// $string['config_default_title'] = 'Navigation Progress';
-// $string['config_group'] = 'Visible only to group or grouping';
-// $string['config_header_monitored'] = 'Monitored';
-// $string['config_icons'] = 'Use icons in bar';
-// $string['config_longbars'] = 'How to present long bars';
-// $string['config_orderby'] = 'Order bar by';
-// $string['config_orderby_course_order'] = 'Ordering in course';
-// $string['config_orderby_due_time'] = 'Time using "{$a}" date';
-// $string['config_percentage'] = 'Show percentage to students';
-// $string['config_scroll'] = 'Scroll';
-// $string['config_selectactivities'] = 'Select activities';
-// $string['config_selectedactivities'] = 'Selected activities';
-// $string['config_squeeze'] = 'Squeeze';
-// $string['config_title'] = 'Alternate title';
-// $string['config_wrap'] = 'Wrap';
-// $string['coursenametoshow'] = 'Course name to show on Dashboard';
-// $string['defaultlongbars'] = 'Default presentation for long bars';
-// $string['forceiconsinbar'] = 'Force icons in the progress bar';
-// $string['fullname'] = 'Full course name';
-// $string['futureNotCompleted_colour'] = '#025187';
-// $string['futureNotCompleted_colour_descr'] = 'HTML colour code for future elements that have not yet been completed';
-// $string['futureNotCompleted_colour_title'] = 'Future not-completed colour';
-// $string['how_activitiesincluded_works'] = 'How including activities works';
-// $string['how_activitiesincluded_works_help'] = '<p>By default, all activities with Activity completion settings set are included in the bar.</p><p>You can also manually select activities to be included.</p>';
-// $string['how_group_works'] = 'How visible group works';
-// $string['how_group_works_help'] = '<p>Selecting a group or grouping will limit the display of the this block to that group or grouping only.</p>';
-// $string['how_longbars_works'] = 'How long bars are presented';
-// $string['how_longbars_works_help'] = '<p>When bars exceed a set length, how they can be presented in one of the following ways.</p><ul><li>Squeezed into one horizontal bar</li><li>Scrolling sideways to show overflowing bar segments</li><li>Wrapping to show all bar segments on multiple lines</li></ul><p>Note that when the bar is wrapped, the NOW indicator will not be shown.</p>';
-// $string['how_ordering_works'] = 'How ordering works';
-// $string['how_ordering_works_help'] = '<p>There are two ways to order activities in the Navigation Progress block.</p><ul><li><em>Time using "Expect completion on" date</em> (default)<br />The expected completion dates of activities/resources are used to order the bar. Where activities/resources don\'t have an expected completion date, ordering in the course is used instead. When this option is used, the NOW indicator is shown.</li><li><em>Ordering in course</em><br />Activities/resources are presented in the same order as they are on the course page. When this option is used, expected completion dates are ignored. When this option is used, the NOW indicator is not shown.</li></ul>';
-// $string['how_selectactivities_works'] = 'How including activities works';
-// $string['how_selectactivities_works_help'] = '<p>To manually select activities to be include in the bar, ensure that "Activities included" is set to "selected activities".</p><p>Only activities with activity completion settings set can be included.</p><p>Hold the CTRL key to select multiple activities.</p>';
-// $string['lastonline'] = 'Last in course';
-// $string['mouse_over_prompt'] = 'Mouse over or touch bar for info.';
-// $string['no_activities_config_message'] = 'There are no activities or resources with activity completion set or no activities or resources have been selected. Set activity completion on activities and resources then configure this block.';
-// $string['no_activities_message'] = 'No activities or resources are being monitored. Use config to set up monitoring.';
-// $string['no_blocks'] = 'No Navigation Progress blocks are set up for your courses.';
-// $string['no_courses'] = "You are not enrolled in any courses. Only bars from enrolled courses will be shown.";
-// $string['no_visible_activities_message'] = 'None of the monitored activities are currently visible.';
-// $string['not_all_expected_set'] = 'Not all activities with completion have an "{$a}" date set.';
-// $string['notCompleted_colour'] = '#C71C22';
-// $string['notCompleted_colour_descr'] = 'HTML colour code for current elements that have not yet been completed';
-// $string['notCompleted_colour_title'] = 'Not-completed colour';
-// $string['now_indicator'] = 'NOW';
+defined('MOODLE_INTERNAL') || die();
+
+$string['completed_colour'] = '#73A839';
+$string['completed_colour_descr'] = 'HTML-код цвета завершенных элементов';
+$string['completed_colour_title'] = 'Цвет завершенного элемента';
+$string['completion_not_enabled'] = 'Отслеживание завершения не включено на этом сайте.';
+$string['completion_not_enabled_course'] = 'Отслеживание завершения не включено в этом курсе.';
+$string['completion_progress:addinstance'] = 'Добавлять новый блок «Ход курса»';
+$string['completion_progress:myaddinstance'] = 'Добавлять блок «Ход курса» на Домашнюю страницу';
+$string['completion_progress:overview'] = 'Просматривать обзор прогресса выполнения всеми студентами';
+$string['completion_progress:showbar'] = 'Показывать панель в блоке «Ход курса»';
+$string['config_activitiesincluded'] = 'Активности включены';
+$string['config_activitycompletion'] = 'Набор всех завершенных элементов курса';
+$string['config_default_title'] = 'Ход курса';
+$string['config_group'] = 'Доступно только для группы или потока';
+$string['config_header_monitored'] = 'Отслеживается';
+$string['config_icons'] = 'Использовать значки на панели';
+$string['config_longbars'] = 'Как показывать длинные панели';
+$string['config_orderby_course_order'] = 'Порядок в курсе';
+$string['config_orderby_due_time'] = 'Время с использованием даты "{$a}"';
+$string['config_percentage'] = 'Показывать процент студентам';
+$string['config_scroll'] = 'Список';
+$string['config_selectactivities'] = 'Выбрать элементы';
+$string['config_selectedactivities'] = 'Выбранные элементы';
+$string['config_squeeze'] = 'Сжать';
+$string['config_title'] = 'Альтернативное название';
+$string['config_wrap'] = 'Перенос';
+$string['coursenametoshow'] = 'Показать название курса в Личном кабинете';
+$string['defaultlongbars'] = 'Представление по умолчанию для длинных панелей';
+$string['fullname'] = 'Полное название курса';
+$string['futureNotCompleted_colour'] = '#025187';
+$string['futureNotCompleted_colour_descr'] = 'HTML-код цвета для будущих элементов, которые еще не завершены';
+$string['futureNotCompleted_colour_title'] = 'Цвет будущих незавершенных';
+$string['how_activitiesincluded_works'] = 'Какие элементы курса включить';
+$string['how_activitiesincluded_works_help'] = '<p> По умолчанию все действия с установленными настройками завершения элемента курса включены в панель.</p><p>Вы также можете вручную выбрать элементы курса для включения. </p>';
+$string['how_group_works'] = 'Для каких групп доступен';
+$string['how_group_works_help'] = '<p>Выбор группы или потока ограничит отображение этого блока только для этой группы или потока.</p';
+$string['how_longbars_works'] = 'Как показывать длинные панели';
+$string['how_longbars_works_help'] = '<p>Когда панели превышают заданную длину, они могут быть показаны одним из следующих способов.</p><ul><li>Сжатыми в одну горизонтальную панель</li><li>С прокруткой в сторону для отображения переполняемых сегментов панели</li><li>С переносом для отображения всех сегментов панели на нескольких линиях</li></ul><p>Обратите внимание, что когда панель перенесена, индикатор «Сейчас» отображаться не будет.</p>';
+$string['how_ordering_works'] = 'Порядок расположения';
+$string['how_ordering_works_help'] = '<p>Существует два варианта расположения элементов курса в блоке «Ход курса».</p><ul><li><em>Используются даты «Ожидаемое время завершения»</em> (по умолчанию)<br />Элементы и ресурсы курса располагаются на панели в порядке ожидаемого времени их завершения.Там, где элементы курса/ресурсы не имеют ожидаемого времени завершения, вместо этого используется их порядок в курсе. Когда используется этот вариант, отображается индикатор СЕЙЧАС.</li><li><em>Порядок в курсе</em><br />Элементы курса/ ресурсы представлены в том же порядке, как и на странице курса. При использовании этого параметра ожидаемое время завершения игнорируется. Когда эта опция используется, индикатор СЕЙЧАС не отображается.</li></ul>';
+$string['how_selectactivities_works'] = 'как включить элементы';
+$string['how_selectactivities_works_help'] = '<p> Чтобы вручную выбрать элементы, которые нужно включить в панель, убедитесь, что для параметра «Включенные элементы» установлено значение «Выбранные элементы». </p> <p> Можно включать только те элементы, для которых установлены параметры их завершения. </p> <p> Удерживайте клавишу CTRL, чтобы выбрать несколько элементов. </p>';
+$string['lastonline'] = 'Последний в курсе';
+$string['mouse_over_prompt'] = 'Наведите курсор мыши или коснитесь панели для получения информации.';
+$string['no_activities_config_message'] = 'Нет элементов или ресурсов с установленным параметром завершения или они не были выбраны. Установите параметр завершения для элементов и ресурсов курса, затем настройте этот блок.';
+$string['no_activities_message'] = 'Никакие элементы курса или ресурсы не отслеживаются. Используйте конфигурацию для настройки контроля.';
+$string['no_blocks'] = 'Блоки «Ход курса» не установлены для ваших курсов.';
+$string['no_courses'] = 'Вы не зачислены ни на один курс. Панели могут быть показаны только участникам курсов.';
+$string['no_visible_activities_message'] = 'Ни один из отслеживаемых элементов курса в настоящее время недоступен.';
+$string['notCompleted_colour'] = '#C71C22';
+$string['notCompleted_colour_descr'] = 'HTML-код цвета для текущих элементов, которые еще не были завершены';
+$string['notCompleted_colour_title'] = 'Цвет для незавершенных элементов курса';
+$string['not_all_expected_set'] = 'Не все элементы с завершением имеют установленную дату "{$a}".';
+$string['now_indicator'] = 'Сейчас';
 $string['overview'] = 'Обзор студентов';
 $string['pluginname'] = 'Ход курса';
-// $string['progress'] = 'Progress';
-// $string['progressbar'] = 'Navigation Progress';
-// $string['privacy:metadata'] = 'The Navigation Progress block only displays existing completion data.';
-// $string['selectitem'] = 'Select \'{$a}\'';
-// $string['shortname'] = 'Short course name';
-// $string['showallinfo'] = 'Show all info';
-// $string['showinactive'] = 'Show inactive students in Overview';
-// $string['showlastincourse'] = 'Show student\'s Last in course time in Overview';
-// $string['submitted'] = 'Submitted';
-// $string['submittednotcomplete_colour'] = '#FFCC00';
-// $string['submittednotcomplete_colour_descr'] = 'HTML colour code for elements that have been submitted, but are not yet complete';
-// $string['submittednotcomplete_colour_title'] = 'Submitted but not complete colour';
-// $string['time_expected'] = 'Expected';
-// $string['why_set_the_title'] = 'Why you might want to set the block instance title?';
-// $string['why_set_the_title_help'] = '<p>There can be multiple instances of the Navigation Progress block. You may use different Navigation Progress blocks to monitor different sets of activities or resources. For instance you could track progress in assignments in one block and quizzes in another. For this reason you can override the default title and set a more appropriate block title for each instance.</p>';
-// $string['why_show_precentage'] = 'Why show a progress percentage to students?';
-// $string['why_show_precentage_help'] = '<p>It is possible to show an overall percentage of progress to students.</p><p>This is calculated as the number of activities completed divided by the total number of activities in the bar.</p><p>The progress percentage appears until the student mouses over an item in the bar.</p>';
-// $string['why_use_icons'] = 'Why you might want to use icons?';
-// $string['why_use_icons_help'] = '<p>You may wish to add tick and cross icons in the Navigation Progress to make this block more visually accessible for students with colour-blindness.</p><p>It may also make the meaning of the block clearer if you believe colours are not intuitive, either for cultural or personal reasons.</p>';
-// $string['wrapafter'] = 'When wrapping, limit rows to';
+$string['privacy:metadata'] = 'Блок «Ход курса» только отображает существующие данные о завершении.';
+$string['progress'] = 'Навигация';
+$string['progressbar'] = 'Ход курса';
+$string['shortname'] = 'Короткое название курса';
+$string['showallinfo'] = 'Показать всю информацию';
+$string['showinactive'] = 'Показать неактивных студентов';
+$string['submitted'] = 'Отправлено';
+$string['submittednotcomplete_colour'] = '#FFCC00';
+$string['submittednotcomplete_colour_descr'] = 'HTML-код цвета для элементов, на которые были отправлены ответы, но они еще не завершены';
+$string['submittednotcomplete_colour_title'] = 'Незавершенные с отправленными ответами';
+$string['time_expected'] = 'Ожидаемый';
+$string['why_set_the_title'] = 'Зачем задавать заголовок блока?';
+$string['why_set_the_title_help'] = '<p>Может быть несколько экземпляров блока «Ход курса». Вы можете использовать разные блоки «Ход курса» для отслеживания различных наборов элементов курса или ресурсов. Например, вы можете отслеживать ход выполнения заданий в одном блоке и тесты в другом. По этой причине вы можете заменить заголовок по умолчанию на более подходящий заголовок для каждого блока.</p>';
+$string['why_show_precentage'] = 'Показывать процент успеваемости студентам?';
+$string['why_show_precentage_help'] = '<p>Можно показать общий процент успеваемости студентов.</p><p>Он рассчитывается как количество выполненных элементов курса, деленное на общее число элементов в панели.</p><p>Процент прогресса отображается до тех пор, пока студент не наведет курсор мыши на элемент в панели.</p>';
+$string['why_use_icons'] = 'Зачем вы можете использовать значки?';
+$string['why_use_icons_help'] = '<p> Возможно, вы захотите добавить значки галочек и крестиков в данный блок, чтобы сделать его более визуально доступным для учащихся с дальтонизмом. </ p>
+<p> Это также может сделать значения блока более понятными, если вы считаете, что цвета не приемлемы по культурным или личным причинам. </ p>';
+$string['wrapafter'] = 'При переносе ограничивайте строки';
